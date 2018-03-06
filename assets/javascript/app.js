ecb686fb0437431f4255f5a31d37215ba9269c16
djemerson <djemerson@gmail.com>
2018-03-05 19:40:07 -0600

    switched to ticketmaster api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,11 @@
 
-<<<<<<< HEAD
 $("#copyright-year").text(moment().year());
 
 // Collapses modal
 $('#btn-primary').on('shown.bs.modal', function () {
     $('#myInput').focus()
   })
-=======
+
 $("#add_artist").on('click', function(event) {
 	event.preventDefault();
 	var artist = $("#artist_name").val().trim();
@@ -15,15 +14,17 @@ $("#add_artist").on('click', function(event) {
 	var location = $("#location").val().trim();
 	location = location.replace(/\s+/g,"+");
 	console.log(location);
-	var eventfulKey = "KP59BCKSVm4x73p7";
+	var ticketmasterKey = "GMb9kWGBfHFrWOyKbZNww60Bsf54F5LU";
+	// var eventfulKey = "KP59BCKSVm4x73p7";
 	var youtubeKey = "AIzaSyD507r0h_zioKfSsE3U407o7pwH85aK3pg";
 	var youtubeDataQuery = "https://www.googleapis.com/youtube/v3/search?key=" + youtubeKey + "&part=snippet&q=" + artist + "&topicId=/m/04rlf";
 	console.log(youtubeDataQuery);
-	var eventfulQuery = "http://api.eventful.com/json/events/search?app_key=" + eventfulKey + "&q=" + artist + "&location=" + location + "&category=concert";
-	console.log(eventfulQuery);
+	var ticketmasterQuery = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + ticketmasterKey + "&keyword=" + artist + "&classificationName=music&city=" + location;
+	// var eventfulQuery = "http://api.eventful.com/json/events/search?app_key=" + eventfulKey + "&q=" + artist + "&location=" + location + "&c=music";
+	// console.log(eventfulQuery);
 
 	$.ajax({
-		url: eventfulQuery,
+		url: ticketmasterQuery,
 		method: 'GET'
 	}).then(function(object) {
 		console.log(object);
@@ -39,4 +40,4 @@ $("#add_artist").on('click', function(event) {
 })
 
 $("#copyright-year").text(moment().year());
->>>>>>> javascript
+
