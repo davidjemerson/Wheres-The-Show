d9b0fb7e5a4c1962e848638e5667e8b3970566cb
Zack Muenz <zackmuenz@gmail.com>
2018-03-10 11:34:34 -0600

    Minor HTML/CSS changes, added play button img overlay

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,7 +44,7 @@ $("#add_artist").on('click', function (event) {
 		method: 'GET'
 	}).then(function (object) {
 		console.log(object);
-		for (var i = 0 ; i < object._embedded.events.length ; i++) {
+		for (var i = 0; i < object._embedded.events.length; i++) {
 			var event = object._embedded.events[i].name;
 			var artist = object._embedded.events[i]._embedded.attractions["0"].name;
 			// console.log("Artist Name: " + artist);
@@ -65,8 +65,8 @@ $("#add_artist").on('click', function (event) {
 
 		if (artistArray.length > 0) {
 			$("#showHolder").html("");
-			for (var i = 0 ; i < artistArray.length ; i++) {
-				var artistBlock = "<li id='" + artistArray[i].artistSearch + "' class='artist-name'><div class='collapsible-header valign-wrapper'><i class='material-icons md-36'>queue_music</i><h5>" + artistArray[i].eventName + "</h5></div><div class='collapsible-body'><div class=row><div class='col m4 s12 center'><a class='vid-link' href='' target=''><img class='thumbnail responsive-img' src=''></img></a><p class='no-margin center-align'>(click thumbnail to open video)</p></div><div class='col m8 s12'><dl><dt class='info'><h6>Info</h6></dt><dd><a href='" + artistArray[i].ticketmasterLink + "' target='_blank'>Event Details at TicketMaster</a></dd><br><dt class='dates'><h6>When</h6></dt><dd>" + moment(artistArray[i].eventDate + " " + artistArray[i].eventTime).format("dddd, MMMM Do YYYY, h:mm a") + "</dd><br><dt class='venue'><h6>Venue</h6></dt><dd>" + artistArray[i].eventVenue + "</dd></dl></div></div></div></li>";
+			for (var i = 0; i < artistArray.length; i++) {
+				var artistBlock = "<li id='" + artistArray[i].artistSearch + "' class='artist-name'><div class='collapsible-header valign-wrapper'><i class='material-icons md-36'>queue_music</i><h5>" + artistArray[i].eventName + "</h5></div><div class='collapsible-body'><div class=row><div class='col m4 s12 center'><a class='vid-link' href='' target=''><div class='video-container'><img class='thumbnail responsive-img' src=''></img><img class='playBtn' src='assets/images/ic_play_circle_outline_white_48dp_2x.png'></img></div></a><p class='no-margin center-align'>(click thumbnail to open video)</p></div><div class='col m8 s12'><dl><dt class='info'><h6>Info</h6></dt><dd><a href='" + artistArray[i].ticketmasterLink + "' target='_blank'>Event Details at TicketMaster</a></dd><br><dt class='dates'><h6>When</h6></dt><dd>" + moment(artistArray[i].eventDate + " " + artistArray[i].eventTime).format("dddd, MMMM Do YYYY, h:mm a") + "</dd><br><dt class='venue'><h6>Venue</h6></dt><dd>" + artistArray[i].eventVenue + "</dd></dl></div></div></div></li>";
 				$("#showHolder").prepend(artistBlock);
 			}
 		}
